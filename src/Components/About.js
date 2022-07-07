e594994a20871749dfe3444481abd1dc952b61ef
agu <iba.agustin@gmail.com>
2022-07-07 18:28:26 -0300

    Modified about, links, style and Nav. Leaderboard addition pending

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,14 +7,18 @@ export default function About() {
         <p className="text">
           Hi! I'm Agu, a junior web developer diving into the development world
           and loving the journey so far. If you'd like to connect, you can find
-          me on Discord :)
+          me on Discord (Jin#6139) or these links! :)
         </p>
         <div className="contact">
-          <ul>
-            <li> - Discord: Jin#6139</li>
+          <ul className="contact-list">
+            <li>
+              <a className="linkedIn-link" href="">
+                <i className="devicon-linkedin-plain"></i>
+              </a>
+            </li>
             <li>
               <a className="gh-link" href="https://github.com/jinitsuga">
-                - GitHub{" "}
+                <i className="devicon-github-original"></i>
               </a>
             </li>
           </ul>
